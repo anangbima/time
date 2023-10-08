e394b1d5f6bfc1ca60928310554119a1b12c241a
anangbima <50908265+anangbima@users.noreply.github.com>
2023-10-08 18:13:08 +0700

    moon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [texture, setTexture] = useState({
+    width : '',
+    height : '',
+    top : '',
+    left : '',
+  });
+
   return (
-    <div className="App">
-      
+    <div className="canvas">
+      {/* <div className='day'>
+
+      </div> */}
+
+      <div className='night'>
+        <div></div>
+        <div className='moon'>
+          <div className='texture-moon'></div>
+          {/* <div className='texture-moon'></div>
+          <div className='texture-moon'></div> */}
+        </div>
+      </div>
     </div>
   );
 }
